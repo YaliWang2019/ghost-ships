68992c17dbb5ca64d73469ece2927bfe98563635
id-uaa <iditmer@alaska.edu>
2021-02-10 17:39:21 -0900

    Clean files and .gitignore

--- game-logic/app/main.cpp
+++ game-logic/app/main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <string>
+#include "../src/Grid.hpp"
 #include "../src/GridAddress.hpp"
 
 int main()
 {
-    std::string address_string("");
+    /*std::string address_string("");
     while (!GridAddress::IsValidInput(address_string)) {
         std::cout << "Enter board position : \n";
         std::cin >> address_string;
-    }
+    }*/
+
+    Grid g;
+
+    std::cout << g.CellStatus(0) << "\n";
+
 
     std::cin.get();
 }
