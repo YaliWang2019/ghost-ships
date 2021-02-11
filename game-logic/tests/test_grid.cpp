318b75cf7e56d23985451e2a77b8b27072fdc05f
id-uaa <iditmer@alaska.edu>
2021-02-10 22:29:30 -0900

    Implement test of Grid.SetShip

--- game-logic/tests/test_grid.cpp
+++ game-logic/tests/test_grid.cpp
@@ -10,3 +10,13 @@ TEST_CASE("New grid reports all cells empty") {
 		REQUIRE(g.CellStatus(i) == "Empty");
 	}
 }
+
+TEST_CASE("Setting cell to ship status functions") {
+
+	Grid g;
+	GridAddress ga("B7");
+
+	bool result = g.SetShip(ga);
+	REQUIRE(result == true);
+	REQUIRE(g.CellStatus(ga) == "Ship");
+}
