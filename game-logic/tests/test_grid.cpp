5545f3923ba1eef9f93004f3c360eb7a674c4c95
isaiah <iditmer@alaska.edu>
2021-02-12 20:08:03 -0900

    Begin placement tests

--- game-logic/tests/test_grid.cpp
+++ game-logic/tests/test_grid.cpp
@@ -1,6 +1,7 @@
 #include "catch.hpp"
 #include "../src/Grid.hpp"
 #include "../src/GridAddress.hpp"
+#include "../src/ShipCollection.hpp"
 
 TEST_CASE("New grid reports all cells empty") {
 
@@ -25,4 +26,19 @@ TEST_CASE("Setting cell to ship status functions") {
 	REQUIRE(g.CellStatus(95) == "Ship");
 
 	REQUIRE_THROWS_AS([&]() { g.SetShip(-1); }(), std::out_of_range);
+}
+
+TEST_CASE("Ship placement test functions") {
+
+	Grid g;
+	Ship s{ "Test_Ship", 4 };
+
+	for (int i = 0; i < 99; i++) {
+		REQUIRE(g.CanPlace(s, i) == true);
+	}
+
+	g.SetShip(2); g.SetShip(12); 
+	g.SetShip(22); g.SetShip(21);
+
+	REQUIRE(g.CanPlace(s, 11) == false);
 }
