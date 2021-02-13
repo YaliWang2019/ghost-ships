115ea3e52bbe60ce54c115081792c9443526b9d9
isaiah <iditmer@alaska.edu>
2021-02-12 22:32:43 -0900

    Quickly output results of placement

--- game-logic/app/main.cpp
+++ game-logic/app/main.cpp
@@ -5,7 +5,39 @@
 
 int main()
 {   
+    {
+        ShipPlacement pl(5);
+        pl.Add(15);
+        pl.Add(25);
+        pl.Finalize();
+
+        std::cout << "START : " << pl.StartPoint().AsString() << "\n";
+        std::cout << "LENGTH: " << pl.Length() << "\n";
+        std::cout << "END   : " << pl.EndPoint().AsString() << "\n";
+    }
+
+
     Grid g;
+    Ship s{ "TestShip",2 };
+    GridAddress ga{ "C3" };
+
+    std::vector<ShipPlacement> places;
+    bool result = g.ValidPlacements(s, ga, places);
+
+    std::cout << " HAS PLACEMENTS : " << result << "\n";
+
+    for (auto& s : places) {
+        for (int i = 0; i < s.Length(); i++) {
+            GridAddress new_add{ s[i] };
+            std::cout << new_add.AsString() << " ";
+        }
+        std::cout << "\n";
+    }
+
+    
+
+
+    /*Grid g;
     bool rerun = true;
 
     std::cout << "\nCurrent Board Positions : " << "\n";
@@ -30,7 +62,7 @@ int main()
         char c;
         std::cin >> c;
         rerun = toupper(c) == 'Y';
-    }
+    }*/
 
 
     std::cin.get();
