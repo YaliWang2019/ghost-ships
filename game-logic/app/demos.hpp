8e359676a6df86ab8307ca6add8edf34b2ded82f
isaiah <iditmer@alaska.edu>
2021-02-17 21:30:36 -0900

    Move all demos from main.cpp to demos.cpp
    To date the code in app/main.cpp was used for
    testing, experimenting with, and demoing functions
    used for the project. All of those have been moved
    into app/demos.cpp so they can be referenced if
    needed, however they are not part of the "final"
    program.

--- game-logic/app/demos.hpp
+++ game-logic/app/demos.hpp
@@ -1,15 +1,17 @@
+#ifndef GHOST_SHIPS_DEMOS_H
+#define GHOST_SHIPS_DEMOS_H
+
 #include <iostream>
 #include <string>
 #include "../src/Adversary.hpp"
 #include "../src/Grid.hpp"
 #include "../src/GridAddress.hpp"
 #include "../src/Turn.hpp"
-#include "demos.hpp"
 
-int main()
-{
-    //PlacementDemo();
-    //FiringDemo();
-    //AutoTurnDemo();
-    AdversaryDemo();
-}
+void PlacementDemo();
+void FiringDemo();
+void AutoTurnDemo();
+void AdversaryDemo();
+
+#endif
+
